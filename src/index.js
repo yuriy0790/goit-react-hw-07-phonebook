1c30209ce5a3fe97a94f813fd5c5d0ba1b9f54b4
Yurii Oborskiy <yuriioborskiy@gmail.com>
2023-01-25 12:47:57 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import 'modern-normalize/modern-normalize.css';
 import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, persistor } from './redux/store';
+import {
+  store,
+  // persistor
+} from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
